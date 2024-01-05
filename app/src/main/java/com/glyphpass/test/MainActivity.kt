f9dd88c2a32f5c3be3b3fed73bdeb525cf0a4bf0
glyphDeveloper <git@glyphpass.com>
2024-01-04 22:39:44 -0700

    update #2

--- app/src/main/java/com/glyphpass/test/MainActivity.kt
+++ app/src/main/java/com/glyphpass/test/MainActivity.kt
@@ -2,6 +2,7 @@ package com.glyphpass.test
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,23 +32,25 @@ class MainActivity : ComponentActivity() {
         Log.d(TAG, "Initializing Firebase Auth")
         val auth = Firebase.auth
 
+        // logUserOut(auth)
+
         /* Calling addSimpleUser() here works as expected IF NO AUTH IS REQUIRED.
         // If the database requires an authenticated user e.g.
         // "allow read, write: if request.auth != null"
         // then calling this method from here will properly fail
         */
-        addSimpleUserToFirestore (db, "Alex")
+        // addSimpleRecordToFirestore (db,"Alex")
 
         /* I can properly add new users to the project whenever needed
         */
         // addFireBaseUserToProject(auth, "jeff3@yahoo.com", "Foo123")
 
-        /*  I cannot write to Firestore if I am authenticated! If no authentication is required,
+        /*  I CANNOT  write to Firestore if I am authenticated! If no authentication is required,
             trying to insert into Firestore will fail silently. If auth is required e.g.
             "allow read, write: if request.auth != null" then it either fails due to inadequate permissions
             or fails silently
         */
-        // authenticateFireBaseUserAndWriteToFirestore(db, auth, "jeff@yahoo.com", "Foo123")
+        authenticateFireBaseUserAndWriteToFirestore(db, auth, "jeff@yahoo.com", "Foo123")
 
 
         setContent {
@@ -63,14 +66,19 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun addFireBaseUserToProject(auth: FirebaseAuth, email: String, password : String) {
+    private fun logUserOut(auth: FirebaseAuth) {
+        Log.d(TAG, "Logging the user out!")
+        auth.signOut()
+    }
+
+
+    private fun addFireBaseUserToProject(auth: FirebaseAuth, email: String, password : String) {
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
-                    // updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w("TAG", "createUserWithEmail:failure", task.exception)
@@ -78,7 +86,7 @@ class MainActivity : ComponentActivity() {
             }
     } // end fun addFireBaseUserToProject()
 
-    fun addSimpleUserToFirestore (db: FirebaseFirestore, firstName : String) {
+    private fun addSimpleRecordToFirestore (db: FirebaseFirestore, firstName : String) {
 
         // Create a new user with a first and last name
         val user = hashMapOf(
@@ -97,6 +105,58 @@ class MainActivity : ComponentActivity() {
                 Log.w(TAG, "Error adding document", e)
             }
     }    // end fun addSimpleUserToFirestore()
+
+
+    private fun authUserAddSimpleRecordToFirestore (db: FirebaseFirestore, auth: FirebaseAuth, firstName : String) {
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to firstName,
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "Authenticated user added Document with ID: ${documentReference.id}")
+                this.logUserOut(auth)
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }    // end fun addSimpleUserToFirestore()
+
+
+    fun authenticateFireBaseUserAndWriteToFirestore(db: FirebaseFirestore, auth: FirebaseAuth, email: String, password : String) {
+
+        Log.d(TAG, "Attempting to login the user")
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "Sign In With Email: Success!")
+                    val user = auth.currentUser
+                    val uid = auth.uid
+                    Log.d(TAG, "User ID is $uid")
+                    Log.d(TAG, "Signed in User  is: $uid")
+                    // Attempt to have the authenticated used write to Firestore
+                    authUserAddSimpleRecordToFirestore(db, auth, "Zillow9")
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    // queryForAllUsers(db)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+
+    }
 
 }   // end class
 
